f1cf9f3fbca564b22fcc03532358fab6008c19ae
CesarAleman182 <alemanmendozac@gmail.com>
2025-05-15 19:12:10 -0600

    Resuelvo tarea

--- semana1/clase1_tarea.js
+++ semana1/clase1_tarea.js
@@ -5,15 +5,16 @@ const { ask } = require('../helpers/input');
 async function main() {
   const num1 = Number(await ask('Ingresa el primer número: '));
   const num2 = Number(await ask('Ingresa el segundo número: '));
+  const num3 =Number(await ask(('Ingresa el tercer numero:')));
 
-  const suma = num1 + num2;
-  const resta = num1 - num2;
-  const multiplicación = num1 * num2;
-  const division = num1 / num2;
+  const suma = num1 + num2 + num3;
+  const resta = num1 - num2 - num2;
+  const multiplicación = num1 * num2 * num3;
+  const division = num1 / num2 / num3;
   // TODO: Implementar la resta, multiplicación y división
 
   console.log('Suma:', suma);
-  console.log('resta:', resta);
+  console.log('Resta:', resta);
   console.log('Multiplicación:', multiplicación);
   console.log('División:', division);
 
